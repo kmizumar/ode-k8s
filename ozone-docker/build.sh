6da92c5da3c1f7e14340e8c96264b870a60f982f
Kiyoshi Mizumaru <kiyoshi.mizumaru@gmail.com>
2022-01-20 17:28:19 +0000

    (tentative) scm service trial

---
+++ ozone-docker/build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -eu
+cd "$(dirname "${BASH_SOURCE[0]}")"
+docker build . -t pfnmaru/ozone-runner
---
+++ scm/bootstrap-scm.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ozone scm --init
+ozone scm --bootstrap

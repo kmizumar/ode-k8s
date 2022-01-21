dd189a77c8ea742f54d79535c5d642fe55b36abf
Kiyoshi Mizumaru <kiyoshi.mizumaru@gmail.com>
2022-01-21 15:21:13 +0000

    skip 'scm --init/--bootstrap' if SCM is already initialized
    primordial SCM just skips 'ozone scm --init' if it's already
    initialized. non-primordial SCM also skips 'ozone scm --bootstrap' if
    it's already initialized but generates tons of annoying messages.

--- scm/bootstrap-scm.sh
+++ scm/bootstrap-scm.sh
@@ -8,5 +8,15 @@ domainname: $(domainname)
 bootstrap script: $(basename $(readlink -f "${BASH_SOURCE[0]}"))
 EOS
 
-ozone scm --init
-ozone scm --bootstrap
+SCM_METAINFO=/data/ozone.scm.db.dirs/scm/current/VERSION
+
+if [ -f "${SCM_METAINFO}" ]; then
+    echo "SCM already initialized. skip both 'scm --init' and 'scm --bootstrap'."
+    echo "--------------------------------"
+    cat ${SCM_METAINFO}
+    echo "--------------------------------"
+    exit 0
+fi
+
+ozone scm --init         # has effects on primordial SCM
+ozone scm --bootstrap    # has effects on non-primordial SCM

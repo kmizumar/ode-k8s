fda8e2f3520fdb590c81418f616d803312b2e6e7
Kiyoshi Mizumaru <kiyoshi.mizumaru@gmail.com>
2022-01-21 11:43:27 +0000

    scm service (3 bare pods, no workloads)

--- scm/bootstrap-scm.sh
+++ scm/bootstrap-scm.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+cat <<EOS
+hostname: $(hostname)
+hostname -f: $(hostname -f)
+domainname: $(domainname)
+bootstrap script: $(basename $(readlink -f "${BASH_SOURCE[0]}"))
+EOS
+
 ozone scm --init
 ozone scm --bootstrap

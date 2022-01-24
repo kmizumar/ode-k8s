c46cf3226b6e5206c78247d30865627fa529fa17
Kiyoshi Mizumaru <kiyoshi.mizumaru@gmail.com>
2022-01-24 09:00:57 +0000

    (tentative) kerberize scm
    clean up

--- security/create-principal.sh
+++ security/create-principal.sh
@@ -2,25 +2,16 @@
 set -euo pipefail
 
 http_princs=$(cat <<EOS
-HTTP/scm-0@EXAMPLE.COM
-HTTP/scm-1@EXAMPLE.COM
-HTTP/scm-2@EXAMPLE.COM
 HTTP/_@EXAMPLE.COM
 EOS
 )
 
 scm_princs=$(cat <<-EOS
-SCM/scm-0@EXAMPLE.COM
-SCM/scm-1@EXAMPLE.COM
-SCM/scm-2@EXAMPLE.COM
 SCM/_@EXAMPLE.COM
 EOS
 )
 
 om_princs=$(cat <<-EOS
-OM/scm-0@EXAMPLE.COM
-OM/scm-1@EXAMPLE.COM
-OM/scm-2@EXAMPLE.COM
 OM/_@EXAMPLE.COM
 EOS
 )

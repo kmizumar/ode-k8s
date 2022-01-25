5e409debd7f6b121c9c217172a99159937b29d06
Kiyoshi Mizumaru <kiyoshi.mizumaru@gmail.com>
2022-01-25 18:08:41 +0000

    (tentative) recon

--- security/create-principal.sh
+++ security/create-principal.sh
@@ -21,6 +21,16 @@ HDDS/_@EXAMPLE.COM
 EOS
 )
 
+recon_princs=$(cat <<-EOS
+RECON/_@EXAMPLE.COM
+EOS
+)
+
+s3g_princs=$(cat <<-EOS
+S3G/_@EXAMPLE.COM
+EOS
+)
+
 function create_spn
 {
     declare princs=${@}
@@ -33,8 +43,12 @@ create_spn ${http_princs}
 create_spn ${scm_princs}
 create_spn ${om_princs}
 create_spn ${dfs_princs}
+create_spn ${recon_princs}
+create_spn ${s3g_princs}
 
 kubectl exec deploy/krb5-server -c krb5-server -- kadmin.local -q 'ktadd -k /var/lib/krb5kdc/HTTP.keytab -glob HTTP/*@EXAMPLE.COM'
 kubectl exec deploy/krb5-server -c krb5-server -- kadmin.local -q 'ktadd -k /var/lib/krb5kdc/SCM.keytab -glob SCM/*@EXAMPLE.COM'
 kubectl exec deploy/krb5-server -c krb5-server -- kadmin.local -q 'ktadd -k /var/lib/krb5kdc/OM.keytab -glob OM/*@EXAMPLE.COM'
 kubectl exec deploy/krb5-server -c krb5-server -- kadmin.local -q 'ktadd -k /var/lib/krb5kdc/HDDS.keytab -glob HDDS/*@EXAMPLE.COM'
+kubectl exec deploy/krb5-server -c krb5-server -- kadmin.local -q 'ktadd -k /var/lib/krb5kdc/RECON.keytab -glob RECON/*@EXAMPLE.COM'
+kubectl exec deploy/krb5-server -c krb5-server -- kadmin.local -q 'ktadd -k /var/lib/krb5kdc/S3G.keytab -glob S3G/*@EXAMPLE.COM'

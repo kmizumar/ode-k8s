db15637f7c46fc433fb9ae82e4076a46abba50f2
Kiyoshi Mizumaru <kiyoshi.mizumaru@gmail.com>
2022-01-26 16:25:47 +0000

    Recon (#3)
    * Recon starts working
    
    need to add `endpoint_pod_names` to Corefile in kube-system/coredns
    ConfigMap:
    
    kubernetes cluster.local in-addr.arpa ip6.arpa {
       pods insecure
       fallthrough in-addr.arpa ip6.arpa
       ttl 30
       endpoint_pod_names
    }

--- security/create-principal.sh
+++ security/create-principal.sh
@@ -2,22 +2,48 @@
 set -euo pipefail
 
 http_princs=$(cat <<EOS
-HTTP/_@EXAMPLE.COM
+HTTP/scm-0.scm-0.default.svc.cluster.local@EXAMPLE.COM
+HTTP/scm-1.scm-1.default.svc.cluster.local@EXAMPLE.COM
+HTTP/scm-2.scm-2.default.svc.cluster.local@EXAMPLE.COM
+HTTP/om-0.om-0.default.svc.cluster.local@EXAMPLE.COM
+HTTP/om-1.om-1.default.svc.cluster.local@EXAMPLE.COM
+HTTP/om-2.om-2.default.svc.cluster.local@EXAMPLE.COM
+HTTP/dn-0.dn-0.default.svc.cluster.local@EXAMPLE.COM
+HTTP/dn-1.dn-1.default.svc.cluster.local@EXAMPLE.COM
+HTTP/dn-2.dn-2.default.svc.cluster.local@EXAMPLE.COM
+HTTP/recon.recon.default.svc.cluster.local@EXAMPLE.COM
+HTTP/s3g.s3g.default.svc.cluster.local@EXAMPLE.COM
 EOS
 )
 
 scm_princs=$(cat <<-EOS
-SCM/_@EXAMPLE.COM
+SCM/scm-0.scm-0.default.svc.cluster.local@EXAMPLE.COM
+SCM/scm-1.scm-1.default.svc.cluster.local@EXAMPLE.COM
+SCM/scm-2.scm-2.default.svc.cluster.local@EXAMPLE.COM
 EOS
 )
 
 om_princs=$(cat <<-EOS
-OM/_@EXAMPLE.COM
+OM/om-0.om-0.default.svc.cluster.local@EXAMPLE.COM
+OM/om-1.om-1.default.svc.cluster.local@EXAMPLE.COM
+OM/om-2.om-2.default.svc.cluster.local@EXAMPLE.COM
 EOS
 )
 
 dfs_princs=$(cat <<-EOS
-HDDS/_@EXAMPLE.COM
+HDDS/dn-0.dn-0.default.svc.cluster.local@EXAMPLE.COM
+HDDS/dn-1.dn-1.default.svc.cluster.local@EXAMPLE.COM
+HDDS/dn-2.dn-2.default.svc.cluster.local@EXAMPLE.COM
+EOS
+)
+
+recon_princs=$(cat <<-EOS
+RECON/recon.recon.default.svc.cluster.local@EXAMPLE.COM
+EOS
+)
+
+s3g_princs=$(cat <<-EOS
+S3G/s3g.s3g.default.svc.cluster.local@EXAMPLE.COM
 EOS
 )
 
@@ -33,8 +59,12 @@ create_spn ${http_princs}
 create_spn ${scm_princs}
 create_spn ${om_princs}
 create_spn ${dfs_princs}
+create_spn ${recon_princs}
+create_spn ${s3g_princs}
 
 kubectl exec deploy/krb5-server -c krb5-server -- kadmin.local -q 'ktadd -k /var/lib/krb5kdc/HTTP.keytab -glob HTTP/*@EXAMPLE.COM'
 kubectl exec deploy/krb5-server -c krb5-server -- kadmin.local -q 'ktadd -k /var/lib/krb5kdc/SCM.keytab -glob SCM/*@EXAMPLE.COM'
 kubectl exec deploy/krb5-server -c krb5-server -- kadmin.local -q 'ktadd -k /var/lib/krb5kdc/OM.keytab -glob OM/*@EXAMPLE.COM'
 kubectl exec deploy/krb5-server -c krb5-server -- kadmin.local -q 'ktadd -k /var/lib/krb5kdc/HDDS.keytab -glob HDDS/*@EXAMPLE.COM'
+kubectl exec deploy/krb5-server -c krb5-server -- kadmin.local -q 'ktadd -k /var/lib/krb5kdc/RECON.keytab -glob RECON/*@EXAMPLE.COM'
+kubectl exec deploy/krb5-server -c krb5-server -- kadmin.local -q 'ktadd -k /var/lib/krb5kdc/S3G.keytab -glob S3G/*@EXAMPLE.COM'
